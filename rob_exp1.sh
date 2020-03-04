4659f9f7b93cfd66afaf293286dcb23cd0cc1a31
Rob Mulla <rob.mulla@gmail.com>
2020-03-04 10:24:46 -0500

    rob_exp1

---
+++ rob_exp1.sh
@@ -0,0 +1,16 @@
+# Rob First experiment with Parker's setup
+python train.py \
+    --model senet50 \
+    --outdir 0304/seresnext50_resize224_rob1/ \
+    --gpu_ids 0,1 \
+    --width 224 \
+    --height 224  \
+    --feather_data_path BengaliData/feather_resize224/ \
+    --mixup 1 \
+    --image_mode gray \
+    --patience 4 \
+    --LR_SCHEDULER REDUCED \
+    --optimizer RADAM \
+    --lr 1e-3 \
+    --lr_ratio 0.65 \
+    --batch_size 128
